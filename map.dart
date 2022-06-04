bed5cc4e2b1601888099055fb187960d7ce28230
AnanyaSamanta09 <ananyasamanta906@gmail.com>
2022-06-04 09:58:56 +0530

    add all functions in map

--- map.dart
+++ map.dart
@@ -1,6 +1,13 @@
 void main() {
-  Map<String, int> map = {'Ananya': 67, 'Vishal': 89, 'Subhojeet': 100};
-  map.forEach((key, value) {
+  Map<String, int> map1 = {'Ananya': 67, 'Vishal': 89, 'Subhojeet': 100};
+  Map<String, int> map2 = {'Puja': 91, 'Sumit': 79, 'Surbhi': 101};
+  map1.addAll(map2);
+  print('map1:');
+  map1.forEach((key, value) {
+    print('key: $key,value:$value');
+  });
+  print('map2:');
+  map2.forEach((key, value) {
     print('key: $key,value:$value');
   });
 }
