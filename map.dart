0570c2591d2e7f53fa2b66573b89f59caca22f91
AnanyaSamanta09 <ananyasamanta906@gmail.com>
2022-06-04 12:41:00 +0530

    using addEntries() function

--- map.dart
+++ map.dart
@@ -1,7 +1,6 @@
 void main() {
   Map<String, int> map1 = {'Ananya': 67, 'Vishal': 89, 'Subhojeet': 100};
   Map<String, int> map2 = {'Puja': 91, 'Sumit': 79, 'Surbhi': 101};
-  map1.addAll(map2);
   print('map1:');
   map1.forEach((key, value) {
     print('key: $key,value:$value');
@@ -10,6 +9,6 @@ void main() {
   map2.forEach((key, value) {
     print('key: $key,value:$value');
   });
-  print('map1.keys:${map1.keys}');
-  print('map1.value:${map1.values}');
+  map1.addEntries(map2.entries);
+  print(map1);
 }
