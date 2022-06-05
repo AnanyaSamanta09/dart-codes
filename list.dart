0ff2eafd771a0ad9b6457a93abdf489e74833f86
AnanyaSamanta09 <ananyasamanta906@gmail.com>
2022-06-05 09:45:03 +0530

    using add() & insert() in List

--- list.dart
+++ list.dart
@@ -7,8 +7,13 @@ void main() {
 
   // 1. insert elements in list using
   // i.  add()
+  stringList.add('Ananya');
+  stringList.add('Khushi');
+  stringList.add('Vishal');
+  print(stringList);
   // ii. insert()
-
+  stringList.insert(1, 'Aperna');
+  print(stringList);
   // 2. remove element using the remove methods
 
   // 3. concatenate two lists of same type
