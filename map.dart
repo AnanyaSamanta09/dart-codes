40a8a48460919b0b2a9de12e65d3586cb059a4f0
AnanyaSamanta09 <ananyasamanta906@gmail.com>
2022-06-06 07:44:20 +0530

    using Map as value in Map

--- map.dart
+++ map.dart
@@ -1,7 +1,7 @@
 void main() {
-  Map<String, List<int>> students = {};
-  students['Ananya'] = [100, 92, 83];
-  students['Khushi'] = [71, 52, 83];
-  students['Aperna'] = [61, 32, 63];
-  print(students.entries);
+  Map<String, Map<String, int>> studentMarks = {};
+  studentMarks['Ananya'] = {'Chemistry': 94, 'Mathematics': 80, 'Computer': 95};
+  studentMarks['Khushi'] = {'Chemistry': 90, 'Mathematics': 70, 'Computer': 90};
+  studentMarks['Aperna'] = {'Chemistry': 50, 'Mathematics': 85, 'Computer': 58};
+  print(studentMarks.entries);
 }
