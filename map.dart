58c603dfb701c752666f09ce7082cb564a13a161
AnanyaSamanta09 <ananyasamanta906@gmail.com>
2022-06-06 07:34:54 +0530

    use of list in map

--- map.dart
+++ map.dart
@@ -1,14 +1,7 @@
 void main() {
-  Map<String, int> map1 = {'Ananya': 67, 'Vishal': 89, 'Subhojeet': 100};
-  Map<String, int> map2 = {'Puja': 91, 'Sumit': 79, 'Surbhi': 101};
-  print('map1:');
-  map1.forEach((key, value) {
-    print('key: $key,value:$value');
-  });
-  print('map2:');
-  map2.forEach((key, value) {
-    print('key: $key,value:$value');
-  });
-  map2.clear();
-  print(map2);
+  Map<String, List<int>> students = {};
+  students['Ananya'] = [100, 92, 83];
+  students['Khushi'] = [71, 52, 83];
+  students['Aperna'] = [61, 32, 63];
+  print(students.entries);
 }
